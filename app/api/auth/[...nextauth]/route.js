800a6b8203fe0cc14aa1da655bc1c8aaaaab7523
Shabin-k <helloshabink@gmail.com>
2023-06-11 13:40:01 +0530

    Database connection

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import { connectToDB } from "@utils/database";
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -9,7 +11,19 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+      //chec if a user already exits
+
+      //if not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
