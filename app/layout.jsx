2fe3c198c087f9d99b87cff1ac974b5d366526be
Shabin-k <helloshabink@gmail.com>
2023-06-11 12:22:03 +0530

    navbar design completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptvilla",
@@ -10,13 +12,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,9 +6,10 @@ import { useEffect, useState } from "react";
 import { useSession, signIn, signOut, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProvider = async () => {
@@ -32,8 +33,6 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-
-      {/* Mobile Navigation */}
 
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
@@ -72,8 +71,64 @@ const Nav = () => {
         )}
       </div>
 
+      {/* Mobile Navigation */}
+
       <div className="sm:hidden flex relative">
-        
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="profile picture"
+              width={37}
+              height={37}
+              className="rounded-full"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-1 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  className="black_btn"
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
       </div>
     </nav>
   );
