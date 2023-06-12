c17df41702d6d780f718c76f7b7bd140ded8829a
Shabin-k <helloshabink@gmail.com>
2023-06-12 21:42:01 +0530

    create prompt router

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
