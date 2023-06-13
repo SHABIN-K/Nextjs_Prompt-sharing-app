166465a240e26c53a1f91cdc04d868fa0d0272f4
Shabin-k <helloshabink@gmail.com>
2023-06-13 20:29:59 +0530

    Prompt route fixed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,6 +8,7 @@ import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
+  const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
