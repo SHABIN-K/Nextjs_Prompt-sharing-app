e8bbc9003db23a31b24061ff2d562908a0daff17
Shabin-k <helloshabink@gmail.com>
2023-06-13 20:54:47 +0530

    feed setup

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("")
 
-export default Feed
+  const handleSearchChange = (e) => {}
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text"
+        placeholder="Search for a tag or a username"
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className="search_input peer" />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;
