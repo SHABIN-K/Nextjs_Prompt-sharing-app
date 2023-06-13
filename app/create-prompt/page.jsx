e358629075ab6495ec7814578865e8293c2e37d0
Shabin-k <helloshabink@gmail.com>
2023-06-13 19:53:18 +0530

    fetched prompt api

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,13 +7,35 @@ import { useSession } from "next-auth/react";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
