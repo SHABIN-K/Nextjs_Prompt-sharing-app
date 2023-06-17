07afabf04103d47ed65e64c2710b2fd5d1f999d7
Shabin-k <helloshabink@gmail.com>
2023-06-17 20:22:13 +0530

    add router in my profile page and create get,delete,patch code in backend

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const [myPosts, setMyPosts] = useState([]);
