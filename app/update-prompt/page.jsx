66943b673674c45ba6642935fdabfa800f3e9056
Shabin-k <helloshabink@gmail.com>
2023-06-19 11:34:50 +0530

    update propmt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,49 +1,64 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
 const EditPrompt = () => {
   const router = useRouter();
-  const { data: session } = useSession();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
   const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
+  const [post, setPatchPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`,{
+        method: "GET"
       });
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
+      const data = await response.json();
+      setPatchPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  //const createPrompt = async (e) => {
+  //  e.preventDefault();
+  //  setSubmitting(true);
+//
+  //  try {
+  //    const response = await fetch("/api/prompt/new", {
+  //      method: "POST",
+  //      body: JSON.stringify({
+  //        prompt: post.prompt,
+  //        userId: session?.user.id,
+  //        tag: post.tag,
+  //      }),
+  //    });
+  //    if (response.ok) {
+  //      router.push("/");
+  //    }
+  //  } catch (error) {
+  //    console.log(error);
+  //  } finally {
+  //    setSubmitting(false);
+  //  }
+  //};
   return (
     <Form
       type="Edit"
       post={post}
-      setPost={setPost}
+      setPost={setPatchPost}
       submitting={submitting}
-      handleSubmit={createPrompt}
+      handleSubmit={()=>{}}
     />
   );
 };
