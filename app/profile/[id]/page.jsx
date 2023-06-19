1abd9c10fba6ce2a76f6599ad117821780b8b2f7
Shabin-k <helloshabink@gmail.com>
2023-06-19 17:06:05 +0530

    profile setup completed and create loading page

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,52 +1,30 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const UserProfile = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
 
-  const [myPosts, setMyPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const response = await fetch(`/api/users/${params?.id}/posts`);
       const data = await response.json();
-      setMyPosts(data);
+      setUserPosts(data);
     };
-    if (session?.user.id) fetchPosts();
-  }, []);
+    if (params?.id) fetchPosts();
+  }, [params.id]);
 
-  const handleEdit = (post) => {
-    router.push(`/update-prompt?id=${post._id}`);
-  };
-
-  const handleDelete = async (post) => {
-    const hasConfirmed = confirm("Are you sure want to delete this prompt?!");
-
-    if (hasConfirmed) {
-      try {
-        await fetch(`/api/prompt/${post._id.toString()}`, {
-          method: "DELETE",
-        });
-        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
-        setMyPosts(filteredPosts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
   return (
     <Profile
-      name="My"
-      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
-      data={myPosts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
+      name={`${userName}'s`}
+      desc={`Welcome to ${userName}'s personalized profile page`}
+      data={userPosts}
     />
   );
 };
