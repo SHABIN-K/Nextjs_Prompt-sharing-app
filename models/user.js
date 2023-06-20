ccc57a8c514a91c277eff5cc3a56bc075c64dedb
Shabin-k <helloshabink@gmail.com>
2023-06-20 12:26:49 +0530

    "Updated username validation regex 8-20 into 4-20 characters
    Revised the regular expression used to validate usernames to require a minimum of 4 characters instead of 8. The expression still ensures that the username contains only alphanumeric characters and certain special characters, is between 4-20 characters in length, does not have consecutive underscores or periods, and does not start or end with either of these characters. If the username fails to match this pattern, an error message will be displayed. This helps prevent security issues like injection attacks and unauthorized access."

--- models/user.js
+++ models/user.js
@@ -10,8 +10,8 @@ const UserSchema = new Schema({
     type: String,
     required: [true, "Username is required!"],
     match: [
-      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+      /^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 4-20 alphanumeric letters and be unique!",
     ],
   },
   image: {
